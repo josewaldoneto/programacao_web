b48da83c79929754e01c02a9371f91990fe33b1a
josewaldoneto <jose.waldo@sempreceub.com>
2024-08-13 09:29:47 -0300

    Implementando funções para 'script.js'

---
+++ script.js
@@ -0,0 +1,14 @@
+let dom = document;
+
+let titulo = document.getElementById("titulo").value;
+let idade = document.getElementById("idade");
+let cpf = document.getElementById("cpf");
+
+console.log(titulo);
+console.log(idade);
+console.log(cpf);
+
+function submeter() {
+    let nome = document.getElementById("Nome").value;
+    console.log(nome);
+}
