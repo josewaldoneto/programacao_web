6e3e31acc0637b5d3989bda3f01ecdcf8b80cbf4
josewaldoneto <jose.waldo@sempreceub.com>
2024-08-13 10:50:18 -0300

    Implementação de validações de CPF

--- script.js
+++ script.js
@@ -21,13 +21,21 @@ function validaCPF(cpf) {
     }
     cpf = cpf.trim();
 
+    // Valida se o CPF possui letras
     if(/[a-zA-Z]/.test(cpf)) {
         alert("CPF não pode conter letras!");
         return false;
     }
 
+    // Valida se o CPF possui alguma coisa alem de numeros ou ponto ou hifen
     if (!/^[\d.-]+$/.test(cpf)) {
         alert("CPF só pode conter números, pontos e hífens!")
+        return false;
+    }
+
+    // Validar que o cpf contem 11 caracteres ou 14(quando se inclui pontos e hifen)
+    if(cpf.length != 11 && cpf.length != 14) {
+        alert("Formato do CPF inválido!");
         return false;
     }
 
