76d86108469ff04368d2d91c7a826bce60fc4f99
josewaldoneto <jose.waldo@sempreceub.com>
2024-08-13 10:34:11 -0300

    Implementando validações para CPF em 'script.js'

--- script.js
+++ script.js
@@ -1,22 +1,33 @@
 let dom = document;
 
-let titulo = document.getElementById("titulo").value;
+// let titulo = document.getElementById("titulo").value;
 
-console.log(titulo);
+// console.log(titulo);
 
 function submeter() {
-    let nome = document.getElementById("nome").value;
-    let idade = document.getElementById("idade").value;
+    // let nome = document.getElementById("nome").value;
+    // let idade = document.getElementById("idade").value;
     let cpf = document.getElementById("cpf").value;
-    console.log(nome);
-    console.log(idade);
-    console.log(cpf);
+    // console.log(nome);
+    // console.log(idade);
+    // console.log(cpf);
     console.log(validaCPF(cpf));    
 }
 
 function validaCPF(cpf) {
     if (cpf == "") {
         alert("Campo CPF não pode ser vazio!");
+        return false;
+    }
+    cpf = cpf.trim();
+
+    if(/[a-zA-Z]/.test(cpf)) {
+        alert("CPF não pode conter letras!");
+        return false;
+    }
+
+    if (!/^[\d.-]+$/.test(cpf)) {
+        alert("CPF só pode conter números, pontos e hífens!")
         return false;
     }
 
